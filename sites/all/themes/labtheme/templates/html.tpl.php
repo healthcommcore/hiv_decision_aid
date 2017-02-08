c98b44c8ef30ab11840ac14acd4f66060a5b62a1
hccadmin <dave_rothfarb@dfci.harvard.edu>
2017-02-08 16:21:01 -0500

    Added Google Analytics code

--- sites/all/themes/labtheme/templates/html.tpl.php
+++ sites/all/themes/labtheme/templates/html.tpl.php
@@ -72,6 +72,18 @@
   <?php print $scripts; ?>
   <script src="https://use.typekit.net/fxc6sme.js"></script>
   <script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+  <!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-91497512-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
   <!--[if lt IE 9]>
     <body class="<?php print $classes . ' ieclass'; ?>" <?php print $attributes;?>>
